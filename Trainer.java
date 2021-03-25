f03ae0f127496ffb727d729ffe492a0fbc790720
vidya <vidyamurugan1818gmail.com>
2021-03-25 12:15:13 +0530

    added

---
+++ Trainer.java
@@ -0,0 +1,25 @@
+public class Trainer
+{
+String dept = "java";
+String institute = "payilagam";
+public Trainer(String dept,String institute)
+{
+this.dept= dept;
+this.institute= institute;
+} 
+private int salary = 10000;
+
+public int getSalary(int salary)
+{
+  return this.salary; 
+}
+public void training()
+{
+System.out.println("java training");
+}
+Trainer trainerkumare = new Trainer(dept,institute);
+public void trainerkumare(String CSE,String payilagam)
+{
+	
+}
+}
