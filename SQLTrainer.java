593cf5606e2fba02a16911586e326102b8cce720
vidya <vidyamurugan1818gmail.com>
2021-03-26 06:01:22 +0530

    new

---
+++ SQLTrainer.java
@@ -0,0 +1,14 @@
+public class SQLTrainer extends Trainer
+{
+public static void main(String args[])
+{
+SQLTrainer ram = new SQLTrainer();
+ram.getSalary();
+ram.trainerkumar();
+ram.training();
+}
+public void practice(String writting,String reading)
+{
+
+}	
+}
