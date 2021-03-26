f69bd70b23e822041db1ad8ff7fb720bd5b8c09b
vidya <vidyamurugan1818gmail.com>
2021-03-26 09:36:34 +0530

    scenario2

---
+++ Actor.java
@@ -0,0 +1,13 @@
+public interface Actor
+{
+boolean makeupRequired = true;
+String address = "chennai";
+public void address();
+	//return address;
+
+
+void act();
+void dance();
+void sing();
+
+}
